996a03e2673739a9583fc4dbd2f87b075adca790
phuongcode5656 <123959392+phuongcode5656@users.noreply.github.com>
2024-09-16 23:24:41 +0700

    day code len

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
+import DrawerNavigator from './routes';
+import Favorites from './screens/Favorites';
+import User from './screens/User';
+import Options from './screens/Options';
+import Store from './store';
+import { Provider } from 'react-redux';
+const App=()=>
+{return(
+  <Provider store={Store}>
+  <DrawerNavigator/>
+  </Provider>
+  );
+  }
+  export default App;
